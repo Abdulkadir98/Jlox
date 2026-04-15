c5954fb040be6ebdf27a2738ea426489347489d1
Abdul Kadir Olia <akolia@amazon.com>
2026-04-14 21:28:06 -0400

    Add Sanning of single character lexemes

---
+++ src/com/craftinginterpreters/lox/Token.java
@@ -0,0 +1,21 @@
+package com.craftinginterpreters.lox;
+
+public class Token {
+    final TokenType type;
+    final String lexeme;
+    final Object literal;
+    final int line;
+
+
+    public Token(TokenType tokenType, String lexeme, Object literal, int line) {
+        this.type = tokenType;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    @Override
+    public String toString() {
+        return type + " " + lexeme + " " + literal;
+    }
+}
