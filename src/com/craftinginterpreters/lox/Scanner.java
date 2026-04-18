d24cba8455e6678cad52b4ceabb0797eec8b6d42
Abdul Kadir Olia <akolia@amazon.com>
2026-04-18 12:05:21 -0400

    Add GenerateAST.java which is a program that generates the Expr.java file and contains classes to represent the expressions in the Syntax tree

--- src/com/craftinginterpreters/lox/Scanner.java
+++ src/com/craftinginterpreters/lox/Scanner.java
@@ -155,14 +155,14 @@ public class Scanner {
         return source.charAt(current + 1);
     }
 
+    private boolean isAlphanumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
     private boolean isAlpha(char c) {
         return (c >= 'a' && c <= 'z') ||
                 (c >= 'A' && c <= 'Z') ||
                 c == '_';
-    }
-
-    private boolean isAlphanumeric(char c) {
-        return isAlpha(c) || isDigit(c);
     }
 
     private boolean isDigit(char c) {
